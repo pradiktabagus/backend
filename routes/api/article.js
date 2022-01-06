a6b0cf1e175cff2108fe6732551b266c2608ce03
pradiktabagus <pradiktabagus@gmail.com>
2022-01-06 17:02:25 +0700

    update article

--- routes/api/article.js
+++ routes/api/article.js
@@ -5,7 +5,14 @@ const Article = require("../../model/Article");
 const User = require("../../model/user");
 
 router.param("article", function (req, res, next, slug) {
-  //   Article
+  Article.findOne({ slug: slug })
+    .then(function (article) {
+      if (!article) return res.sendStatus(404);
+      req.article = article;
+
+      return next();
+    })
+    .catch(next);
 });
 
 router.post("/", auth, function (req, res, next) {
@@ -27,15 +34,15 @@ router.post("/", auth, function (req, res, next) {
 
 router.get("/feeds", auth, function (req, res, next) {
   const { limit, offset } = req.query;
-  let lim = limit ? Number(limit) : 20;
+  let lim = limit ? Number(limit) : 10;
   let off = offset ? Number(offset) : 0;
-
+  let skipIdx = (off - 1) * lim;
   User.findById(req.user.id)
     .then(function (user) {
       if (!user) {
         return res.sendStatus(401);
       }
-      Promise.all([Article.find().limit(lim).skip(off), Article.count()])
+      Promise.all([Article.find().limit(lim).skip(skipIdx), Article.count()])
         .then(function (result) {
           let article = result[0];
           let totalArticle = result[1];
@@ -54,6 +61,22 @@ router.get("/feeds", auth, function (req, res, next) {
     .catch((error) => {
       res.send({ message: "Error in Fetching user" });
     });
+});
+
+router.get("/:article", auth, function (req, res, next) {
+  Promise.all([
+    req.user ? User.findById(req.user.id) : null,
+    Article.findById(req.article.id),
+  ])
+    .then(function (result) {
+      let user = result[0];
+      let article = result[1];
+      return res.json({
+        status: 200,
+        data: article,
+      });
+    })
+    .catch(next);
 });
 
 module.exports = router;
